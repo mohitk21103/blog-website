430b7b672464c1a2ebb51b71871680ff43c08dd8
Mohit Kanojiya <mohitk@gmail.com>
2024-08-12 17:47:32 +0530

    Initial commit

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ from forms import CreatePostForm
 from forms import RegisterForm
 from forms import LoginForm, CommentForm
 from flask import abort
+import os
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
+
+csrf = CSRFProtect(app)
+
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -46,7 +51,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")  # 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
